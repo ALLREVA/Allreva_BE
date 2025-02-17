463b3ef7eb8b4d68c0125998cf3de921d8fb2540
sangcci <pshsh910@gmail.com>
2025-02-17 15:35:12 +0900

    [ARV-223] test: rentJoinApplyTest 테스트 코드 수정

--- src/test/java/com/backend/allreva/rent/integration/RentJoinApplyTest.java
+++ src/test/java/com/backend/allreva/rent/integration/RentJoinApplyTest.java
@@ -1,10 +1,18 @@
 package com.backend.allreva.rent.integration;
 
+import static com.backend.allreva.rent.fixture.RentFixture.createRentFixture;
+import static com.backend.allreva.rent.fixture.RentJoinApplyRequestFixture.createRentJoinApplyRequestFixture;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 import com.backend.allreva.rent.command.application.RentCommandService;
 import com.backend.allreva.rent.command.domain.RentRepository;
 import com.backend.allreva.rent_join.command.application.RentJoinCommandService;
 import com.backend.allreva.rent_join.command.domain.RentJoin;
 import com.backend.allreva.rent_join.command.domain.RentJoinRepository;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,12 +20,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static com.backend.allreva.rent.fixture.RentJoinApplyRequestFixture.createRentJoinApplyRequestFixture;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -38,8 +40,15 @@ class RentJoinApplyTest {
         // given
         var memberId = 1L;
         var rentId = 1L;
+        var rent = createRentFixture(memberId, rentId);
         var rentJoinApplyRequest = createRentJoinApplyRequestFixture(rentId);
-        given(rentRepository.existsById(rentJoinApplyRequest.rentId())).willReturn(true);
+        given(rentJoinRepository.existsByBoardingDateAndRentIdAndMemberId(
+                rentJoinApplyRequest.boardingDate(),
+                rentJoinApplyRequest.rentId(),
+                memberId
+        )).willReturn(false);
+        given(rentRepository.findById(rentId)).willReturn(Optional.of(rent));
+        given(rentJoinRepository.countRentJoin(rentId, rentJoinApplyRequest.boardingDate())).willReturn(0);
         given(rentJoinRepository.save(any(RentJoin.class))).willAnswer(invocation -> invocation.getArgument(0));
 
         // when
