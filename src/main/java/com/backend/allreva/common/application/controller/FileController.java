15dd5780bb27472bc941cb16708a65650ed0981d
dowoo kim <dowoo12345@naver.com>
2025-02-25 11:40:55 +0900

    fix : persigned-url url 주소 수정

--- src/main/java/com/backend/allreva/common/application/controller/FileController.java
+++ src/main/java/com/backend/allreva/common/application/controller/FileController.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/avi/v1/file")
+@RequestMapping("/api/v1/file")
 @RequiredArgsConstructor
 public class FileController implements FileControllerSwagger{
     private final FileService fileService;
